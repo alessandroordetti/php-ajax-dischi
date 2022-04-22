40c54d4d0064ed22b56be37897c910c189ae40c8
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-22 16:00:32 +0200

    echo test for data dischi array

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <link rel="stylesheet" href="style.css">
     <title>PHP Dischi</title>
 </head>
 <body>
+    <?php 
+        include __DIR__ . "/server/data.php";
+    ?>
+
+
     <header>
         <div class="img-wrapper">
             <img src="img/Spotify.png" alt="Logo Spotify">
@@ -17,7 +23,19 @@
     </header>
 
     <main>
-        
+        <div>
+            <?php 
+                foreach ($dischi as $element) {
+                    echo $element['poster'] . '<br>' .
+                    $element['title'] . '<br>' .
+                    $element['autore'] . '<br>' .
+                    $element['genere'] . '<br>' .
+                    $element['anno'];
+                }
+            ?>
+        </div>
     </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </body>
 </html>
