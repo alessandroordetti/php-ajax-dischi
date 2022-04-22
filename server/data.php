10cba4f8fa81c37c1c5e44576f750ab01363f9fd
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-22 15:48:32 +0200

    Setting Exercise

---
+++ server/data.php
@@ -0,0 +1,72 @@
+<?php $dischi=[
+    [
+        "poster" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+        "title" => "New Jersey",
+        "autore"=> "Bon Jovi",
+        "genere"=> "Rock",
+        "anno" => "1988"
+    ],
+    [
+        "poster" => "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+        "title" => "Live at Wembley 86",
+        "autore"=> "Queen",
+        "genere"=> "Pop",
+        "anno" => "1989"
+    ],
+    [
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+        "title" => "Ten's Summoner's Tales",
+        "autore"=> "Sting",
+        "genere"=> "Pop",
+        "anno" => "1990"
+    ],
+    [
+        "poster" => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+        "title" => "Steve Gadd Band",
+        "autore"=> "Steve Gadd Band",
+        "genere"=> "Jazz",
+        "anno" => "1988"
+    ],
+    [
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+        "title" => "Brave new World",
+        "autore"=> "Iron Maiden",
+        "genere"=> "Metal",
+        "anno" => "1988"
+    ],
+    [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+        "title" => "One more car, one more raider",
+        "autore"=> "Eric Clapton",
+        "genere"=> "Rock",
+        "anno" => "1988"
+    ],
+    [
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+        "title" => "Made in Japan",
+        "autore"=> "Deep Purple",
+        "genere"=> "Rock",
+        "anno" => "1988"
+    ],
+    [
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+        "title" => "And Justice for All",
+        "autore"=> "Metallica",
+        "genere"=> "Metal",
+        "anno" => "1988"
+    ],
+    [
+        "poster" => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+        "title" => "Hard Wired",
+        "autore"=> "Dave Weckl",
+        "genere"=> "Jazz",
+        "anno" => "1988"
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+        "title" => "Bad",
+        "autore"=> "Michael Jacjson",
+        "genere"=> "Pop",
+        "anno" => "1988"
+    ],
+] ?>
